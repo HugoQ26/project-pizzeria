af344f636f94e7181e763213eec60b311d9e742d
Marcin Kubiak <mkubiak73@gmail.com>
2019-11-01 09:45:35 +0100

    modify template

--- src/js/script.js
+++ src/js/script.js
@@ -6,7 +6,8 @@
 
   const select = {
     templateOf: {
-      menuProduct: '#template-menu-product'
+      menuProduct: '#template-menu-product',
+      cartProduct: '#template-cart-product' // CODE ADDED
     },
     containerOf: {
       menu: '#product-list',
@@ -27,18 +28,44 @@
     },
     widgets: {
       amount: {
-        input: 'input[name="amount"]',
+        input: 'input.amount', // CODE CHANGED
         linkDecrease: 'a[href="#less"]',
         linkIncrease: 'a[href="#more"]'
       }
+    },
+    // CODE ADDED START
+    cart: {
+      productList: '.cart__order-summary',
+      toggleTrigger: '.cart__summary',
+      totalNumber: `.cart__total-number`,
+      totalPrice:
+        '.cart__total-price strong, .cart__order-total .cart__order-price-sum strong',
+      subtotalPrice: '.cart__order-subtotal .cart__order-price-sum strong',
+      deliveryFee: '.cart__order-delivery .cart__order-price-sum strong',
+      form: '.cart__order',
+      formSubmit: '.cart__order [type="submit"]',
+      phone: '[name="phone"]',
+      address: '[name="address"]'
+    },
+    cartProduct: {
+      amountWidget: '.widget-amount',
+      price: '.cart__product-price',
+      edit: '[href="#edit"]',
+      remove: '[href="#remove"]'
     }
+    // CODE ADDED END
   };
 
   const classNames = {
     menuProduct: {
       wrapperActive: 'active',
       imageVisible: 'active'
+    },
+    // CODE ADDED START
+    cart: {
+      wrapperActive: 'active'
     }
+    // CODE ADDED END
   };
 
   const settings = {
@@ -46,13 +73,23 @@
       defaultValue: 1,
       defaultMin: 1,
       defaultMax: 9
+    }, // CODE CHANGED
+    // CODE ADDED START
+    cart: {
+      defaultDeliveryFee: 20
     }
+    // CODE ADDED END
   };
 
   const templates = {
     menuProduct: Handlebars.compile(
       document.querySelector(select.templateOf.menuProduct).innerHTML
+    ),
+    // CODE ADDED START
+    cartProduct: Handlebars.compile(
+      document.querySelector(select.templateOf.cartProduct).innerHTML
     )
+    // CODE ADDED END
   };
 
   class AmountWidget {
